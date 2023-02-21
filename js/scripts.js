de4049b554500fb95966de65414c2778a760001b
Justin Shaffer <justinbshaffer91@gmail.com>
2023-02-20 20:03:21 -0800

    finished implementing test logic into business logic

--- js/scripts.js
+++ js/scripts.js
@@ -59,11 +59,13 @@ function testGetAvailableMoves() {
 testGetAvailableMoves();
 
 // ********** Business Logic **********
+
 const playerX = 'X';
 const playerO = 'O';
 
 let currentPlayer = playerX;
 
+// Creates an empty board
 // Creates an empty board
 function createBoard() {
   return [
@@ -73,17 +75,6 @@ function createBoard() {
   ];
 }
 
-// Returns true if the board is full, false otherwise
-function isBoardFull(board) {
-  for (let row = 0; row < 3; row++) {
-    for (let col = 0; col < 3; col++) {
-      if (board[row][col] === null) {
-        return false;
-      }
-    }
-  }
-  return true;
-}
 
 // Plays a move on the board
 function play(row, column) {
@@ -93,7 +84,11 @@ function play(row, column) {
 
 // Returns the player that will move next
 function getNextPlayer(currentPlayer) {
-  return currentPlayer === 'X' ? 'O' : 'X';
+  if (currentPlayer === playerX) {
+    return playerO;
+  } else {
+    return playerX;
+  }
 }
 
 // Returns an array of available moves
@@ -107,4 +102,36 @@ function getAvailableMoves(board) {
     }
   }
   return moves;
+}
+
+// Returns true if the board is full, false otherwise
+function isBoardFull() {
+  return getAvailableMoves(board).length === 0;
+}
+
+// Returns true if the game is over, false otherwise
+function checkWin(player) {
+  // Check rows
+  for (let row = 0; row < 3; row++) {
+    if (board[row][0] === player && board[row][1] === player && board[row][2] === player) {
+      return true;
+    }
+  }
+
+  // Check columns
+  for (let i = 0; i < 3; i++) {
+    if (board[0][i] === player && board[1][i] === player && board[2][i] === player) {
+      return true;
+    }
+  }
+
+  // Check diagonals
+  if (board[0][0] === player && board[1][1] === player && board[2][2] === player) {
+    return true;
+  }
+  if (board[0][2] === player && board[1][1] === player && board[2][0] === player) {
+    return true;
+  }
+
+  return false;
 }
